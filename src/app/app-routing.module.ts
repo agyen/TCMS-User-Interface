1d652996bb51b5d63e57bb616ef0752b7640be82
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2019-12-03 18:17:21 +0000

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomersComponent } from './customers/customers.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReportsComponent } from './reports/reports.component';
+import { DetailsComponent } from './details/details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'home', component: HomeComponent},
+  {path: 'customers', component: CustomersComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path:'reports', component:ReportsComponent},
+  {path:'details', component:  DetailsComponent},
+
+  { path : "", redirectTo:"/home", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CustomersComponent } from './customers/customers.component';
+import { ReportsComponent } from './reports/reports.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    ProjectsComponent,
+    CustomersComponent,
+    ReportsComponent,
+    NavbarComponent,
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
